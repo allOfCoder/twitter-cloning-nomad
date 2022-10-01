bf753e950cd609962c2fdeefedaff64017b71f51
allOfCoder <gotjd0230@naver.com>
2022-10-01 16:03:09 +0900

    1.0.7

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import AppRouter from "components/Router";
-import fbase from "fbase";
+import { authService } from "fbase";
 
 function App() {
-  const auth = fbase.auth();
+  console.log(authService);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return <AppRouter isLoggedIn={isLoggedIn} />;
 }
--- src/fbase.js
+++ src/fbase.js
@@ -1,9 +1,7 @@
+import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
-import { initializeApp } from "firebase/app";
-import * as firebase from "firebase/app";
-import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +10,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-const fbase = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-export default fbase;
+export const firebaseInstance = firebase;
+export const authService = firebase.auth();
