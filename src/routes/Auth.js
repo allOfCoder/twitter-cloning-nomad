bb2625ca343c07cb7a11969a57a555ff275c4a81
allOfCoder <gotjd0230@naver.com>
2022-10-05 16:21:22 +0900

    1.0.8

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { getAuth } from "firebase/auth";
+import { authService } from "fbase";
 
-const Auth = () => <span>Auth?</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const auth = getAuth();
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService
+          .createUserWithEmailAndPassword(auth, email, password)
+          //email 형식이 유효하지 않다고 오류 뜸
+          .then((userCredential) => {
+            const user = userCredential.user;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        data = await authService
+          .signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            const user = userCredential.user;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          onChange={onChange}
+          value={email}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          onChange={onChange}
+          value={password}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
